a790069d62bb601b49dfc614b753b7662a73cca2
techarm <techarmdev@gmail.com>
2023-05-07 10:53:28 +0900

    Webサーバー起動のテストコードを追加

--- todo-api/go.mod
+++ todo-api/go.mod
@@ -1,3 +1,5 @@
 module github.com/techarm/todo-api
 
 go 1.19
+
+require golang.org/x/sync v0.2.0 // indirect
--- todo-api/main.go
+++ todo-api/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}))
-
-	if err != nil {
+	if err := run(context.Background()); err != nil {
 		fmt.Printf("failed to teminate server: %v", err)
 		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別のゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+		}
+		return nil
+	})
+
+	// チャネルからの終了通知を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// 別のゴルーチンの終了をまつ
+	return eg.Wait()
 }
